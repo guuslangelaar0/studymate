d64bf9c7a5cc075856b19c4c0f6af67f7b4c2d03
Guus Langelaar <guuslangelaar@gmail.com>
2020-02-15 00:35:58 +0100

    update flashmessage

--- resources/views/layouts/admin/partials/flash-message.blade.php
+++ resources/views/layouts/admin/partials/flash-message.blade.php
@@ -1,9 +1,15 @@
+<div class="container">
+
+
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
     @if(Session::has($msg))
-        <div class="flash-message">
-            <p class="alert alert-{{ $msg }}">{{ Session::get($msg) }}
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            </p>
+        <div class="row">
+            <div class="flash-message col">
+                <p class="alert alert-{{ $msg }}">{{ Session::get($msg) }}
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                </p>
+            </div>
         </div>
     @endif
 @endforeach
+</div>
